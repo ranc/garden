a225355b5be8691c8f01e00a7515cd56a9009d4b
ranc <ran.a.cohen@gmail.com>
2023-01-15 09:34:52 +0200

    reuse addr

--- server.py
+++ server.py
@@ -108,6 +108,7 @@ class Server:
         
     def wait_for_clients(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             s.bind((HOST, PORT))
             s.listen()
             while not self.stop:
